e5c549af14abcef5d992de4728ce497217a20ea1
MihaiCulbida <clbidamihai@gmail.com>
2026-05-26 08:06:19 +0300

    Testing local server

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Blitziq</title>
+  <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+  <h1>Test</h1>
+  <?php echo "Afisare text in php"; ?>
+
+  <script src="src/script.js"></script>
+</body>
+</html>
