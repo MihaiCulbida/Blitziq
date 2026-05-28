4a078d0b2b5510ecbbd8e0c21a0b0b6bb87f98a9
MihaiCulbida <clbidamihai@gmail.com>
2026-05-28 08:12:15 +0300

    Added first desgin of navbar

--- index.php
+++ index.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
-<html>
+<html lang="ro">
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Blitziq</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
 
-  <button onclick="window.location.href='login.php'">Login</button>
-  <button onclick="window.location.href='register.php'">Register</button>
+  <nav class="navbar">
+    <a class="navbar-logo"> Blitz <span class="navbar-logo-badge">IQ</span> </a>
+
+    <ul class="navbar-links">
+      <li><a href="#">text</a></li>
+      <li><a href="#">text</a></li>
+      <li><a href="#">text</a></li>
+      <li><a href="#">text</a></li>
+      <li><a href="#">text</a></li>
+    </ul>
+
+    <div class="navbar-actions">
+      <button class="navbar-btn navbar-btn-login">Log in</button>
+      <button class="navbar-btn navbar-btn-signup">Sign up</button>
+    </div>
+  </nav>
+
 </body>
 </html>
