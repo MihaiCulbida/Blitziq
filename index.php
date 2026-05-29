aa527b57e41017f13865fa64d25b50bd2731b4ca
MihaiCulbida <clbidamihai@gmail.com>
2026-05-29 11:50:33 +0300

    Day 5

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
   <section class="hero">
 
     <div class="hero-shapes">
-      <div class="blob blob-1"></div>
+      <div class="blob   blob-1"></div>
       <div class="blob blob-2"></div>
       <div class="blob blob-3"></div>
       <div class="blob blob-4"></div>
