20f5c0670dffa05d7d028381487106f622eefd42
MihaiCulbida <clbidamihai@gmail.com>
2026-06-02 10:47:36 +0300

    Started working on home page of the web app

---
+++ page.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<style>
+    .images {
+        display: flex;
+        gap: 20px;
+    }
+
+    .image {
+        width: 30px;
+        height: 30px;
+        object-fit: cover;
+    }
+</style>
+    <div class="images"></div>
+        <img class="image" src="img/home.png" alt="">
+        <img class="image" src="img/book.png" alt="">
+        <img class="image" src="img/discover.png" alt="">
+    </div>
+</body>
+</html>
